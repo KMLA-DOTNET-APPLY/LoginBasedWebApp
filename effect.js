a3c9ca5edf7779fe7a4262170729a06abdcaf164
Jungwoong Kim <dk040106@naver.com>
2019-08-27 19:23:50 +0900

    rechange from index.php to html

--- effect.js
+++ effect.js
@@ -1,19 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Korean Book',
-    image: './assets/korean-book-front.jpg',
-    inStock: true,
-    details: ["KMLA textbook", "Slightly Used", "For students and teachers"],
-    variants: [
-      {
-        variantId: 0001,
-        variantSide: "front"
-      },
-      {
-        variantId: 0002,
-        variantSide: "back"
-      }
-    ],
+    
   }
 })
