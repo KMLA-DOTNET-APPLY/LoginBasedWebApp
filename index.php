395e707ee86804e6e43dfc2384c5386f4382dffb
joyjang3002 <joyjang3002@gmail.com>
2019-08-29 20:47:33 +0900

    login template

--- index.php
+++ index.php
@@ -1,53 +1,35 @@
 <!DOCTYPE html>
-<html lang="ko">
-  <head>
-    <meta charset="UTF-8">
-    <title>김정웅 지원서</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <h1 id='head'>dot.Net</h1>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>로그인 예제</title>
+    </head>
 
-    <div id="content">
-        <h2>인적 사항</h2>
-        <table>
-            <tbody>
-                <tr>
-                    <td>이름</td>
-                    <td>:
-                    <td>장서현</td>
-                </tr>
-                <tr>
-                    <td>기수</td>
-                    <td>:
-                    <td>24기</td>
-                </tr>
-                <tr>
-                    <td>학번</td>
-                    <td>:
-                    <td>191037</td>
-                </tr>
-                <tr>
-                    <td>Gmail</td>
-                    <td>:
-                    <td>dk20040106@gmail.com</td>
-                </tr>
-            </tbody>
-        </table>
+    <body>
+            <h2>닷넷 2차 선발용 로그인 웹 템플릿<h2>
+            <?php
+				session_start();
 
-        <div class="w3-row">
-            <div class="w3-container w3-half">
-                <h2>닷넷에 지원해주신 이유는 무엇입니까?</h2>
-                <p>페이스북에 있는 민사고 그룹들과 기존의 큼온, 인트라넷 등의 기능을 통합한 시스템을 만들어서 학생들의 생활을 개선하기 위함입니다.</p>
-            </div>
-            <div class="w3-container w3-half">
-                <h2>당신은 얼마나 컴퓨터와 친한가요?</h2>
-                <p>중학교 3학년때 C언어를 접하면서 컴퓨터에 관심이 생겼습니다. 학교에서 배운 Java를 포함해서 Python, R, C, HTML, CSS, Swift, Kotlin, Javascript, C#등의 프로그래밍 언어를 접해보았습니다.</p>
-                <p>1학기때는 다양한 것들을 해보는 것을 목표로 웹, 앱, 게임 개발, 머신러닝 등을 공부해 보았고 지금도 계속하고 있습니다.</p>
-            </div>
-        </div>
-    </div>
-  </body>
-</html>
+				if(!isset($_SESSION['id']) || !isset($_SESSION['name']))
+				{
+					echo "<meta http-equiv='refresh' content='0;url=login.html'>";
+					exit;
+				}
+				$id = $_SESSION['id'];
+				$name = $_SESSION['name'];
+
+				echo "<p>안녕하세요. $name($id)님</p>";
+
+				//
+				//contests 여기다 추가하세요
+				//
+
+				echo "<p><a href='logout.php'>로그아웃</a></p>";
+			?>
+        	<a href="signup.html" target="right">
+				<button>회원가입 하러가기</button>
+			</a>
+			
+    </body>
+
+</html> 
--- signup_check.php
+++ signup_check.php
@@ -1,23 +1,29 @@
 <?php
+    
+    require('db.php');
 
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $server = $url["host"];
-    $serverusername = $url["user"];
-    $serverpassword = $url["pass"];
-    $db = substr($url["path"], 1);
-    $mysqli=mysqli_connect($server, $serverusername, $serverpassword, $db);
-
-    $id = $_POST['id']; 
-    $name = $_POST['name']; 
-    $pw = $_POST['pw']; 
-
-    $query = mysqli_query($mysqli,"INSERT INTO user_info(id,name,pw) VALUES('$id', '$name', '$pw')");
-
-    if($query)
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $pw = $_POST['pw'];
+    
+    $check="SELECT *from user_info WHERE userid='$id'";
+    $result=$mysqli->query($check);
+    if($result->num_rows==1)
     {
-        echo "회원가입 성공";
+        echo "중복된 아이디입니다.";
+        echo "<button onclick=\"location.href='signup.html'\"> 돌아가기 </button>";
+        exit();
+    }
+
+    $signup=mysqli_query($mysqli,"INSERT INTO user_info (id,name,pw) VALUES ('$id','$name','$pw')");
+    if($signup)
+    {
+        ?>
+        <meta charset="utf-8" />
+        <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
+        <meta http-equiv="refresh" content="0 url=/">
+        <?php
     }
     else
-        echo "실패";
-
+        echo "<button onclick=\"location.href='signup.html'\"> 회원가입 실패, 돌아가기 </button>";
 ?>
