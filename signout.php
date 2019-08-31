b3c8639d4c0880907bb55a5e37d06a211553beb6
Jungwoong Kim <dk040106@naver.com>
2019-08-31 23:34:27 +0900

    make signout

--- signout.php
+++ signout.php
@@ -1,7 +1,22 @@
 <?php
+    session_start();
+    $id=$_POST['id'];
+    $pw=$_POST['pw'];
 
-$id = $_SESSION['id'];
+    $check="SELECT * FROM user_info WHERE id='$id'";
+    $result=$mysqli->query($check); 
 
-echo $id;
-
+    if($result->num_rows==1)
+    {
+        //id에 correspond하는 하나의 열을 배열로 가져오기
+        $row=$result->fetch_array(MYSQLI_ASSOC);
+    
+        //비밀번호 확인하기
+        if($pw == $row['pw'])
+        {
+            $delete="DELETE * FROM user_info WHERE id='$id'";
+            query($delete);
+            header('Location: ./signout_check.html');
+        }
+    }
 ?>
