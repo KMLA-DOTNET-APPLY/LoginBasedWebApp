a94b27344efa737a1e78fa7ef8c486103116aabf
Jungwoong Kim <dk040106@naver.com>
2019-09-03 22:12:23 +0900

    testing signout.php

--- signout.php
+++ signout.php
@@ -3,20 +3,6 @@
     $id=$_POST['id'];
     $pw=$_POST['pw'];
 
-    $check="SELECT * FROM user_info WHERE id='$id'";
-    $result=$mysqli->query($check);
-
-    if($result->num_rows==1)
-    {
-        //id에 correspond하는 하나의 열을 배열로 가져오기
-        $row=$result->fetch_array(MYSQLI_ASSOC);
-    
-        //비밀번호 확인하기
-        if($pw == $row['pw'])
-        {
-            $delete="DELETE * FROM user_info WHERE id='$id'";
-            query($delete);
-            header('Location: ./signout_check.html');
-        }
-    }
+    echo $id;
+    echo $pw;
 ?>
