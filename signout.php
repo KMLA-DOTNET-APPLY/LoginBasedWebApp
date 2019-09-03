6e35857f26104ff4b3c3f91425d1532471236e97
Jungwoong Kim <dk040106@naver.com>
2019-09-03 22:29:34 +0900

    update signout.php

--- signout.php
+++ signout.php
@@ -1,8 +1,23 @@
 <?php
     session_start();
-    $id=$_POST['id'];
-    $pw=$_POST['pw'];
+    $id_input=$_POST['id'];
+    $pw_input=$_POST['pw'];
 
-    echo $id;
-    echo $pw;
+    require('db.php');
+
+    $check="SELECT * FROM user_info WHERE id='$id'";
+    $delete="DELETE * FROM user_info WHERE id='$id'";
+    $result=$mysqli->query($check); 
+
+    if($result->num_rows==1)
+    {
+         //id에 correspond하는 하나의 열을 배열로 가져오기
+        $row=$result->fetch_array(MYSQLI_ASSOC);
+        
+         //비밀번호 확인하기
+        if($pw_input == $row['pw'] && $id_input == $row['id'])
+        {
+            query($delete);
+        }
+    }    
 ?>
