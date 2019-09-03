c2060da66a8540d1011a58f76c0f8b2775849ea6
Jungwoong Kim <dk040106@naver.com>
2019-09-03 22:35:32 +0900

    update signout.php

--- signout.php
+++ signout.php
@@ -3,6 +3,11 @@
     $id_input=$_POST['id'];
     $pw_input=$_POST['pw'];
 
+    echo $id_input;
+    echo "<br>";
+    echo $pw_input;
+    echo "<br>";
+
     require('db.php');
 
     $check="SELECT * FROM user_info WHERE id='$id'";
@@ -14,20 +19,25 @@
          //id에 correspond하는 하나의 열을 배열로 가져오기
         $row=$result->fetch_array(MYSQLI_ASSOC);
         
+        echo $row['id'];
+        echo "<br>";
+        echo $row['pw'];
+        echo "<br>";
+
          //비밀번호 확인하기
-        if($pw_input == $row['pw'] && $id_input == $row['id'])
-        {
-            query($delete);
-            header('Location: ./index.php');
-            exit();
-        }
+        //if($pw_input == $row['pw'] && $id_input == $row['id'])
+        //{
+        //    query($delete);
+        //    header('Location: ./index.php');
+        //    exit();
+        //}
     }    
     
 ?>
 <script type="text/javascript">
-    alert('탈퇴 실패');
-    <?php 
-        unset($_POST);
-    ?>
-    window.location = 'login.html';
+//    alert('탈퇴 실패');
+//    <?php 
+//        unset($_POST);
+//    ?>
+//    window.location = 'login.html';
 </script>
