f7ca64a07e896d105b4eac8c8aa7c30f37e2b82b
Jungwoong Kim <dk040106@naver.com>
2019-09-04 23:22:35 +0900

    update signout.php

--- signout.php
+++ signout.php
@@ -1,32 +1,17 @@
 <?php
-    session_start();
     $id_input=$_POST['id'];
     $pw_input=$_POST['pw'];
 
     require('db.php');
 
-    $check="SELECT * FROM user_info WHERE id='$id'";
-    $result=$mysqli->query($check); 
-
-    $row=$result->fetch_array(MYSQLI_ASSOC);
-        
-    echo $row['id'];
-    echo "<br>";
-    echo $row['pw'];
-    echo "<br>";
-    echo $pw_input;
-
     $delete="DELETE * FROM user_info WHERE id='$id'";
 
-         //비밀번호 확인하기
-        //if($pw_input == $row['pw'] && $id_input == $row['id'])
-        //{
-        //    query($delete);
-        //    header('Location: ./index.php');
-        //    exit();
-        //}
-    //}    
-    
+    if($pw_input == $_SESSION['pw'] && $id_input == $_SESSION['id'])
+    {
+        query($delete);
+        header('Location: ./index.php');
+        exit();
+    }    
 ?>
 <script type="text/javascript">
     alert('탈퇴 실패');
