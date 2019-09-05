7656a061aac1e7b81e31bb894193c7ebb8d99263
dk040106 <48355099+dk040106@users.noreply.github.com>
2019-09-05 20:04:47 +0900

    Update signout.php

--- signout.php
+++ signout.php
@@ -6,14 +6,27 @@
 
     require('db.php');
 
-    echo $id_input;
+    //delete 명령어 만들기
     $delete="DELETE FROM user_info WHERE id=$id_input";
-    mysqli_query($mysqli, $delete);
+
+    //session의 id값과 pw값이 입력한 값과 같으면 탈퇴를 진행한다.
+    if($_SESSION['id'] == $id_input)
+    {
+        if($_SESSION['pw'] == $pw_input)
+        {
+            //데이터를 지운다
+            mysqli_query($mysqli, $delete);
+            //세션 변수를 초기화한다
+            unset($_SESSION);
+            header('Location: ./index.php');
+            exit();
+        }
+    }
 ?>
 <script type="text/javascript">
-    //alert('탈퇴 실패');
+    alert('탈퇴 실패');
     <?php 
-        //unset($_POST);
+        unset($_POST);
     ?>
-    //window.location = 'index.php';
+    window.location = 'index.php';
 </script>
