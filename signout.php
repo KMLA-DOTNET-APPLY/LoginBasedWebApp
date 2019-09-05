ab9046f7d95b308e1514351261f99fb11db728fa
Jungwoong Kim <dk040106@naver.com>
2019-09-05 21:58:27 +0900

    bug fixes

--- signout.php
+++ signout.php
@@ -19,7 +19,7 @@
             $res=session_destroy(); //모든 세션 변수 지우기 = 로그인된 정보 모두 파괴
             if($res) 
             {
-                header('Location: ./signout_check.php'); // 로그아웃 성공 시 로그인 페이지로 이동
+                header('Location: ./signout_check.html'); // 로그아웃 성공 시 로그인 페이지로 이동
             }
             exit();
         }
