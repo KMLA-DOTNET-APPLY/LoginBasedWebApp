8bbf0f0015e8d7731cc4cfa8822ea880a04181fd
joyjang3002 <joyjang3002@gmail.com>
2019-09-07 19:33:45 +0900

    update memberfind

--- member_find_id.php
+++ member_find_id.php
@@ -14,11 +14,12 @@
     //입력하면, 이름이 일치 했을 때, 아이디를 보여준다
 
     //입력한 이름과 일치하는 array를 가져온다
-    $sql = "SELECT * FROM member user_info WHERE name = $name_input";
+    $check="SELECT * FROM user_info WHERE 'name' = $name_input";
+    $result=$mysqli->query($check);
+    $row=$result->fetch_array(MYSQLI_ASSOC);
 
     if($_SESSION['name'] == $name_input){ //일치하는 이름이 있을 때
-        $result = $sql -> fetch_array(); 
-	    echo "<script>alert('회원님의 ID는 "$result['id']" 입니다.'); history.back();</script>";
+	    echo "<script>alert('회원님의 ID는 "$row['id']" 입니다.'); history.back();</script>";
     }else{ //일치하는 이름이 없을 떄
     echo "<script>alert('없는 계정입니다.'); history.back();</script>";
     }
