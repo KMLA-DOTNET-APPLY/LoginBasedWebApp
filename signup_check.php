127064cde51cf082b9e022f102c64aa5264187a9
Jungwoong Kim <dk040106@naver.com>
2019-09-08 10:59:57 +0900

    add comments

--- signup_check.php
+++ signup_check.php
@@ -1,34 +1,51 @@
 <?php
-    
+    //필요한 정보($mysqli)를 db.php에서 가져온다.
     require('db.php');
 
+    //입력값을 받는다. 
     $id = $_POST['id'];
     $user = $_POST['user'];
     $pw = $_POST['pw'];
+
+    //입력하지 않았으면 경고를 띄우고 페이지를 다시 로드한다.
     if($id == "" || $user == "" || $pw == "")
     {
-	    echo '<script> alert("항목을 입력하세요"); history.back(); </script>';
-    }
-    $check="SELECT * from user_info WHERE id=$id";
-    $result = mysqli_query($mysqli, $check);
-    if($result->num_rows==1)
-    {
-        echo "중복된 아이디입니다.";
-        echo "<button onclick=\"location.href='signup.html'\"> 돌아가기 </button>";
-        $result->close();
+        echo '<script> alert("항목을 입력하세요"); history.back(); </script>';
         exit();
     }
 
-    $insert = "INSERT INTO user_info (id,user,pw) VALUES ('$id','$user','$pw')";
-    $signup = mysqli_query($mysqli, $insert);
-    if($signup)
+    //입력된 id값으로 database에서 검색한다.
+    $check="SELECT * from user_info WHERE id=$id";
+    $result = mysqli_query($mysqli, $check);
+
+    //동일한 id값이 존재하면 경고를 띄우고 페이지를 다시 로드한다.
+    if($result->num_rows==1)
     {
-        ?>
-        <meta charset="utf-8" />
-        <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
-        <meta http-equiv="refresh" content="0 url=/">
-        <?php
+        echo '<script> alert("중복된 아이디입니다"); history.back(); </script>';
+
+        //mysqli_result를 해제한다. (php 공식 문서에 나와있는 방법)
+        $result->close();
+
+        exit();
     }
-    else
-        echo "<button onclick=\"location.href='signup.html'\"> 회원가입 실패, 돌아가기 </button>";
+    else //동일한 id값이 존재하니 않는 경우
+    {
+        //데이터베이스에 값을 입력한다. 
+        $insert = "INSERT INTO user_info (id,user,pw) VALUES ('$id','$user','$pw')";
+        $signup = mysqli_query($mysqli, $insert);
+
+        //더 이상 사용하지 않으므로 데이버에이스를 닫는다.
+        $mysqli->close();
+        //정상적으로 작동했을 경우
+        if($signup)
+        {
+            ?>
+            <meta charset="utf-8" />
+            <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
+            <meta http-equiv="refresh" content="0 url=/">
+            <?php
+        }
+        else
+            echo "<button onclick=\"location.href='signup.html'\"> 회원가입 실패, 돌아가기 </button>";
+    }
 ?>
