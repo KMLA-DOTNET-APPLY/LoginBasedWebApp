90ea9fe0c77351581f678f29362c71435c6a239b
Jungwoong Kim <dk040106@naver.com>
2019-09-08 16:15:29 +0900

    bug fixes

--- signout/signout.php
+++ signout/signout.php
@@ -20,7 +20,7 @@
                 //모든 세션 변수 지우기 = 로그인된 정보 모두 파괴
                 if(session_destroy()) 
                 {
-                    header('Location: ../index.html'); // 로그아웃 성공 시 로그인 페이지로 이동
+                    header('Location: ../index.php'); // 로그아웃 성공 시 로그인 페이지로 이동
                 }
                 unset($_POST);
                 exit();
