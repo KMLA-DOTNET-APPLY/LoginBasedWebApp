bdead48785449c9a41adca73e77a048899d9751f
Jungwoong Kim <dk040106@naver.com>
2019-09-18 23:11:49 +0900

    bug fixes

--- login/login_check.php
+++ login/login_check.php
@@ -2,8 +2,10 @@
     session_start();
     $id=$_POST['id'];
     $pw=$_POST['pw'];
+    echo 1;
 
     require('../db/db.php');
+    echo 2;
     
     $check="SELECT * FROM user_info WHERE id=$id";
     $result=$mysqli->query($check);
@@ -11,15 +13,17 @@
     {
         echo '<script> alert("항목을 입력하세요"); history.back(); </script>';
     }
+    echo 3;
 
     if($result->num_rows==1)
     {
          //id에 correspond하는 하나의 열을 배열로 가져오기
         $row=$result->fetch_array(MYSQLI_ASSOC);
-        
+        echo 4;
          //비밀번호 확인하기
         if($pw == $row['pw'])
         {
+            echo 5;
             $user = $row['user'];
             //비밀번호 확인됐으면 세션 변수 만들기
             $_SESSION['id']=$id;
@@ -27,7 +31,7 @@
             $_SESSION['pw']=$pw;
 
             
-            //세션 변수에 등록 잘 됐는지 확인
+            /*//세션 변수에 등록 잘 됐는지 확인
             if(isset($_SESSION['id']) && isset($_SESSION['user']) && isset($_SESSION['pw'])) 
             {
                 //로그인 성공 시 페이지 이동
@@ -36,12 +40,12 @@
             }
         }
     }
-    unset($_POST);
+    unset($_POST);*/
 ?>
 <script type="text/javascript">
-    alert('로그인 실패');
+    //alert('로그인 실패');
     <?php 
-        unset($_POST);
+        //unset($_POST);
     ?>
-    window.location = './login.html';
+    //window.location = './login.html';
 </script>
