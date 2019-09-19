5e906cf3b0ae41eb57e396fbb77c643ba88c8ed0
Jungwoong Kim <dk040106@naver.com>
2019-09-19 10:37:52 +0900

    bug fixing

--- login/login_check.php
+++ login/login_check.php
@@ -18,9 +18,10 @@
     $check="SELECT * FROM user_info WHERE id=$id";
     $result=$mysqli->query($check);
 
+    echo 3;
 
     $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo 3;
+    echo 4;
 
     /*if($result->num_rows==1)
     {
