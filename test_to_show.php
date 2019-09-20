71504f2d78c5b1ff64d6e131490073278559992a
Jungwoong Kim <dk040106@naver.com>
2019-09-21 08:58:47 +0900

    bug fixes

--- test_to_show.php
+++ test_to_show.php
@@ -16,7 +16,7 @@
         echo "$row[1] <br>";
         echo "$row[2] <br>";
         echo date("Y년 m월 d일 h:i:sa", $row[3]);
-        echo "Likes"."$row[4]";
+        echo "<br> Likes"."$row[4]";
     }
 
     /*newsfeed_data에 포함된 모든 글을 가져온다.
