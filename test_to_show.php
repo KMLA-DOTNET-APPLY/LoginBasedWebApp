f9a5dfe8f1e9f1140fbf8b4230ae61cdb15ece5a
Jungwoong Kim <dk040106@naver.com>
2019-09-21 08:57:39 +0900

    bug fixes

--- test_to_show.php
+++ test_to_show.php
@@ -8,14 +8,14 @@
     
     foreach ($rows as $row)
     {
-        foreach ($row as $content)
+        /*foreach ($row as $content)
         {
             echo "$content <br>";
-        }
+        }*/
         echo "$row[0] <br>";
         echo "$row[1] <br>";
         echo "$row[2] <br>";
-        echo data("Y년 m월 d일 h:i:sa", $row[3]);
+        echo date("Y년 m월 d일 h:i:sa", $row[3]);
         echo "Likes"."$row[4]";
     }
 
