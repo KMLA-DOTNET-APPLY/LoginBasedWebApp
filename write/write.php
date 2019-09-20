c48247cb62ff0a7443af52537002ab581f2ca56d
Jungwoong Kim <dk040106@naver.com>
2019-09-20 14:43:20 +0900

    bug fixes

--- write/write.php
+++ write/write.php
@@ -24,21 +24,22 @@
         echo '<script> alert("글을 작성하는 것에 실패하였습니다."); history.back(); </script>';
     }
     
-    /*--포스트로 받은 값을 받아서
-    --값이 없으면 경고를 한다
-    --데이터베이스에 들어갈 수 있는 형식으로 만든 다음
-    --author, title, content, created_at, like_num, id
+    /*  포스트로 받은 값을 받아서
+    *   값이 없으면 경고를 한다
+    *   데이터베이스에 들어갈 수 있는 형식으로 만든 다음
+    *   author, title, content, created_at, like_num, id
     
-    author: session에 있는 사용자 id
-    title: 입력받은 제목
-    content: 입력받은 내용
-    created_at: time()값을 사용하고 -> int 값이니까 비교도 됨 
-    like_num: 일단 0으로 하고 추가할수록 늘리는 걸로
-    id: 작성 시간이랑 작성자의 id
+    *   author: session에 있는 사용자 id
+    *   title: 입력받은 제목
+    *   content: 입력받은 내용
+    *   created_at: time()값을 사용하고 -> int 값이니까 비교도 됨 
+    *   like_num: 일단 0으로 하고 추가할수록 늘리는 걸로
+    *   id: 작성 시간이랑 작성자의 id
     
-    --데이터베이스에 작성한다
-    --성공적인 경우
-    --index.html로 돌아간다*/
+    *   데이터베이스에 작성한다
+    *   성공적인 경우
+    *   index.html로 돌아간다
+    */
 ?>
 
 
