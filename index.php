9194ad9feaff9c32e1573dd187b74a548d188c9d
Jungwoong Kim <dk040106@naver.com>
2019-09-21 14:19:53 +0900

    bug fixes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
     if(!isset($_SESSION['id']) || !isset($_SESSION['user']) || !isset($_SESSION['pw']))
     {
-        echo '<script>alert("로그인이 필요합니다."); window.location = "./login/login.html"; </script>';
+        header("Location: ./login/login.html");
         exit();
     }
 ?>
