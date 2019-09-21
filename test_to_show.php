85b6fbfa72e9a5817c0f9426c5a458cb000bee8d
Jungwoong Kim <dk040106@naver.com>
2019-09-21 09:00:31 +0900

    bug fixes

--- test_to_show.php
+++ test_to_show.php
@@ -8,15 +8,11 @@
     
     foreach ($rows as $row)
     {
-        /*foreach ($row as $content)
-        {
-            echo "$content <br>";
-        }*/
         echo "$row[0] <br>";
         echo "$row[1] <br>";
         echo "$row[2] <br>";
         echo date("Y년 m월 d일 h:i:sa", $row[3]);
-        echo "<br> Likes"."$row[4]";
+        echo "<br> Likes: "."$row[4] <br>";
     }
 
     /*newsfeed_data에 포함된 모든 글을 가져온다.
