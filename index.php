84eb6135bf98b2640f2c4c65dd7f481589bb36d9
Jungwoong Kim <dk040106@naver.com>
2019-09-23 14:38:16 +0900

    bug fixes

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
                 echo "<p>$row[2]</p><br>";
                 echo date("Y년 m월 d일 h:i:sa", $row[3]);
                 echo "<br> Likes: " . "$row[4]<br><br>";
+                echo "<input type='button'>"; // 댓글을 쓰는 버튼
             }
             /*newsfeed_data에 포함된 모든 글을 가져온다.
             화면에 각각의 글들을 가지고 와서 표시를 한다.
