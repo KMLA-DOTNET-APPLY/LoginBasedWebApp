7f06f642dac8e872bf708d1d887cbae0ad4dcd14
Jungwoong Kim <dk040106@naver.com>
2019-10-26 10:14:16 +0900

    bug fixes

--- index.php
+++ index.php
@@ -66,9 +66,9 @@ echo 5;*/
             ?>*/
         ?>
         <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>
+        <!-- <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>
 
-        <!-- TODO: Add SDKs for Firebase products that you want to use https://firebase.google.com/docs/web/setup#available-libraries -->
+        TODO: Add SDKs for Firebase products that you want to use https://firebase.google.com/docs/web/setup#available-libraries
 
         <script>
         // Your web app's Firebase configuration
@@ -83,6 +83,6 @@ echo 5;*/
         };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
-        </script>
+        </script>-->
     </body>
 </html>
